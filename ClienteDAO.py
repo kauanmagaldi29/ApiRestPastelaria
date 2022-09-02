92154486b2a0e6efda6b18ad1c57b88399f20a55
Kauan Magaldi <kauanmagaldi29@gmail.com>
2022-09-02 08:59:59 -0300

    atividade

--- ClienteDAO.py
+++ ClienteDAO.py
@@ -1,14 +1,31 @@
-from flask_restful import Resource
+from fastapi import APIRouter
+from pydantic import BaseModel
 
-# Criar os endpoints de Funcionario: GET, POST, PUT, DELETE
+router = APIRouter()
 
-class Cliente(Resource):
+class Cliente(BaseModel):
+    codigo: int = None
+    nome: str
+    matricula: str
+    cpf: str
+    telefone: str = None
+    grupo: int
+    senha: str = None
 
-    def get(self, id):
-        return {"msg": "get executado"}, 200
-    def post(self, id):
-        return {"msg": "post executado"}, 201
-    def put(self, id):
-        return {"msg": "put executado"}, 201
-    def delete(self, id):
-        return {"msg": "delete executado"}, 201
+# Criar os endpoints de Cliente: GET, POST, PUT, DELETE
+
+@router.get("/cliente/{id}", tags=["cliente"])
+def get_cliente(id: int):
+    return {"msg": "get executado"}, 200
+
+@router.post("/cliente/{id}", tags=["cliente"])
+def post_cliente(id: int):
+    return {"msg": "post executado"}, 200
+
+@router.put("/cliente/{id}", tags=["cliente"])
+def put_cliente(id: int):
+    return {"msg": "put executado"}, 201
+
+@router.delete("/cliente/{id}", tags=["cliente"])
+def delete_cliente(id: int):
+    return {"msg": "delete executado"}, 201
